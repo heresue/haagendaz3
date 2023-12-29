409a6edfc05285be28f33ccde69075a6f214fbdb
heresue <sue.synpark@gmail.com>
2023-12-29 18:39:27 +0900

    12/29ez

--- assets/js/comm.js
+++ assets/js/comm.js
@@ -1,7 +1,17 @@
-js;
 new Swiper(".swiper-container", {
     direction: "vertical", // 수직 슬라이드
     autoplay: true, // 자동 재생 여부
     loop: true, // 반복 재생 여부
   });
 
+
+var swiper = new Swiper(".swiper-btm", {
+  direction: "vertical",
+  slidesPerView: 1,
+  spaceBetween: 30,
+  mousewheel: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
